5d803dc40a8950e0970ed6e7fd844cb4798278de
Genilda <146936379+genilda-pixel@users.noreply.github.com>
2024-03-16 17:51:09 +0000

    Add project files.

---
+++ CestasCafés.Domain/Entities/Villa.cs
@@ -0,0 +1,16 @@
+﻿namespace CestasCafés.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
+
